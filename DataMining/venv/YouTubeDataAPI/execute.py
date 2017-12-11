e99277f5a007ea2339633078bc9d4e5bdb0212a2
Rustam <rustam.atakisiev@gmail.com>
2017-12-11 12:04:17 +0100

    pycharm_2

---
+++ DataMining/venv/YouTubeDataAPI/execute.py
@@ -0,0 +1,15 @@
+from __future__ import division
+from datetime import datetime
+import requests
+from lxml import html, etree
+import json
+from textblob import TextBlob
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import warnings
+
+
+api_key = "AIzaSyBwQ6N1_r5r2UyT2LtQJgQbHtFjrtZl7w8"
