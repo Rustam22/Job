99807ca6dfc27b35a263dcb2503852777b123fa0
Rustam199! <atakishiyev.1772090@studenti.uniroma1.it>
2017-12-18 14:53:12 +0100

    Python Django

---
+++ PythonDjango/django/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rustam/Desktop/Projects/Job/PythonDjango/django/venv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
