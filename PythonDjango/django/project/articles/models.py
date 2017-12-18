645c8d8c00a35b33208c23435ae67ff147d66636
Rustam199! <atakishiyev.1772090@studenti.uniroma1.it>
2017-12-18 17:59:43 +0100

    articles

---
+++ PythonDjango/django/project/articles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Article(models.Model):
+    title = models.TextField(max_length=254)
+    body = models.TextField()
+    like = models.IntegerField()
+
+
+
+class Comment(models.Model):
+    #article = models.ForeignKey(Article)
+    text = models.TextField()
+
+
