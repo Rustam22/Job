f72669e7c488b5ff69af4e81bfe076b2f8826de0
Rustam199! <atakishiyev.1772090@studenti.uniroma1.it>
2017-12-21 18:23:33 +0100

    Test Migration

--- PythonProject/django/newproject/accounts/models.py
+++ PythonProject/django/newproject/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    email = models.TextField(max_length=524)
+    password = models.TextField(max_length=254)
+    age = models.IntegerField()
+
+
