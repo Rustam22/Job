ac013705f1f6471c7bda114d076bcc8333df2e6e
Rustam199! <atakishiyev.1772090@studenti.uniroma1.it>
2017-12-28 13:40:43 +0100

    Replace

--- PythonProject/django/newproject/articles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('hello/', views.hello, name='hello'),
-    path('template/', views.helloTemplate, name='helloTemplate'),
-    path('classTemplate/', views.HelloTemplate.as_view(), name='helloClassTemplate'),
-]
