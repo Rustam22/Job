63acd6e976d5ada8d128e9d2258b64faca0ae3aa
Rustam199! <atakishiyev.1772090@studenti.uniroma1.it>
2018-01-02 18:47:36 +0100

    Revert "Basemanager Last Updata(success)"
    This reverts commit 3d40eba5885ca0e3db3afa7b7a5110a9daa59fb2.

--- PythonProject/django/newproject/accounts/admin.py
+++ PythonProject/django/newproject/accounts/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import (
-    UserForm, User
-)
+from .models import UserForm
 
 # Register your models here.
 admin.site.register(UserForm)
-admin.site.register(User)
