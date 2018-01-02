3d40eba5885ca0e3db3afa7b7a5110a9daa59fb2
Rustam199! <atakishiyev.1772090@studenti.uniroma1.it>
2018-01-02 18:47:36 +0100

    Basemanager Last Updata(success)

--- PythonProject/django/newproject/accounts/admin.py
+++ PythonProject/django/newproject/accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import UserForm
+from .models import (
+    UserForm, User
+)
 
 # Register your models here.
 admin.site.register(UserForm)
+admin.site.register(User)
