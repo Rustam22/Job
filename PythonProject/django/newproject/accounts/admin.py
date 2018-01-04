a782ec7166022cd921768f43ccbd8410926c3c44
Rustam199! <atakishiyev.1772090@studenti.uniroma1.it>
2018-01-04 14:14:13 +0100

    Revert "Revert "Manual incomplete UserForm Model""
    This reverts commit a0e49378aa3afa8ad14447bd81bbfc0da7a8ae41.

--- PythonProject/django/newproject/accounts/admin.py
+++ PythonProject/django/newproject/accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile, UserForm
+from .models import (
+    UserForm, User
+)
 
 # Register your models here.
 admin.site.register(UserProfile)
